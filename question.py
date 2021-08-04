51f5b3d071edf0c28631aaaa1fd6e56ccabfdd72
IzabelaMacalino <86921708+IzabelaMacalino@users.noreply.github.com>
2021-08-04 15:26:53 +0100

    Delete question.py

--- question.py
+++
@@ -1,5 +0,0 @@
-#questions class will be able to store the qestion and also be able to store the question answer
-class question:
-     def __init__(self, q_prompt, q_answer):
-          self.q_prompt = q_prompt;
-          self.q_answer= q_answer;
